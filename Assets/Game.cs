45e8eae222a487a63fbe58f15754f91079fc63fc
Nikolaos N <ict.edu1@gmail.com>
2020-04-26 16:26:52 +0300

    Initial Commit

---
+++ Assets/Game.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public Transform prefab;
+    public KeyCode createKey = KeyCode.C;
+    public KeyCode newGameKey = KeyCode.N;
+    public KeyCode saveKey = KeyCode.S;
+    public KeyCode loadKey = KeyCode.L;
+
+    private List<Transform> objects;
+    string savePath;
+
+    void Awake()
+    {
+        objects = new List<Transform>();
+        savePath = Application.persistentDataPath;
+        savePath = Path.Combine(Application.persistentDataPath, "saveFile");
+        Debug.Log(savePath);
+    }
+
+    void Save()
+    {
+        using (
+            var writer = new BinaryWriter(File.Open(savePath, FileMode.Create)))
+        {
+            writer.Write(objects.Count);
+            for (int i = 0; i < objects.Count; i++)
+            {
+                Transform t = objects[i];
+                writer.Write(t.localPosition.x);
+                writer.Write(t.localPosition.y);
+                writer.Write(t.localPosition.z);
+            }
+        }
+        void Load()
+        {
+            BeginNewGame();
+            using (
+                var reader = new BinaryReader(File.Open(savePath, FileMode.Open)))
+            {
+                int count = reader.ReadInt32();
+                for (int i = 0; i < count; i++)
+                {
+                    Vector3 p;
+                    p.x = reader.ReadSingle();
+                    p.y = reader.ReadSingle();
+                    p.z = reader.ReadSingle();
+                    Transform t = Instantiate(prefab);
+                    t.localPosition = p;
+                    objects.Add(t);
+                }
+            }
+        }
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(createKey))
+            {
+                CreateObject();
+            }
+            else if (Input.GetKey(newGameKey))
+            {
+                BeginNewGame();
+            }
+            else if (Input.GetKeyDown(saveKey))
+            {
+                Save();
+            }
+            else if (Input.GetKeyDown(loadKey))
+            {
+                Load();
+            }
+        }
+    }
+
+    void BeginNewGame()
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            Destroy(objects[i].gameObject);
+        }
+        objects.Clear();
+    }
+
+    void CreateObject()
+    {
+        Transform t = Instantiate(prefab);
+        t.localPosition = UnityEngine.Random.insideUnitSphere * 5f;
+        t.localRotation = UnityEngine.Random.rotation;
+        t.localScale = Vector3.one * UnityEngine.Random.Range(0.1f, 1f);
+        objects.Add(t);
+    }
+
+}
